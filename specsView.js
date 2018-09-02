549c10ee37f5bd1ffc532a029f33d11f715f0e8b
noxafy <hci@gmx.de>
2018-09-02 11:52:12 +0200

    Generate sliders dynamically

--- specsView.js
+++ specsView.js
@@ -37,58 +37,73 @@ SpecsView = {
 		}
 	},
 	LightChanger: {
-		sliders: document.getElementById("light_props_slider"),
+		sliders: document.getElementById("light_props_sliders"),
 		enable(light) {
-			registerSlider("intensity", light.intensity, function (val) {
+			this.registerSlider("intensity", light.intensity, function (val) {
 				return light.intensity = val;
-			});
-			registerSlider("constant", Math.sqrt(light.constant), function (val) {
+			}, 5);
+			this.registerSlider("constant", Math.sqrt(light.constant), function (val) {
 				return light.constant = Math.pow(val, 2);
 			});
-			registerSlider("linear", Math.cbrt(light.linear), function (val) {
+			this.registerSlider("linear", Math.cbrt(light.linear), function (val) {
 				return light.linear = Math.pow(val, 3);
 			});
-			registerSlider("quadratic", Math.pow(light.quadratic, 1 / 4), function (val) {
+			this.registerSlider("quadratic", Math.pow(light.quadratic, 1 / 4), function (val) {
 				return light.quadratic = Math.pow(val, 4);
 			});
 
-			function registerSlider(id, default_val, onchange) {
-				const el = document.getElementById(id);
-				const label = document.getElementById(id + "_label");
-				el.onchange = function () {
-					let val = onchange(el.value);
-					label.innerText = id + ": " + getRoundedValue(val);
-				}
-				el.value = default_val;
-				let val = onchange(default_val);
-				label.innerText = id + ": " + getRoundedValue(val);
-
-				function getRoundedValue(val) {
-					if (val > 1e-1) {
-						return Math.round(val * 1000) / 1000
-					}
-					else if (val > 1e-2) {
-						return Math.round(val * 10000) / 10000
-					}
-					else if (val > 1e-3) {
-						return Math.round(val * 100000) / 100000
-					}
-					else if (val > 1e-4) {
-						return Math.round(val * 1000000) / 1000000
-					}
-					else if (val > 1e-5) {
-						return Math.round(val * 10000000) / 10000000
-					}
-					else if (val > 1e-6) {
-						return Math.round(val * 100000000) / 100000000
-					}
-					else return val;
-				}
-			}
-
 			this.sliders.className = "";
 		},
+		registerSlider(id, default_val, set, max = 1, min = 0, step = 0.001) {
+			// label
+			const label = document.createElement("label");
+			label.for = id;
+			label.id = id + "_label";
+			this.sliders.appendChild(label);
+
+			// input element
+			const el = document.createElement("input");
+			el.type = "range";
+			el.value = default_val;
+			el.min = min;
+			el.max = max;
+			el.step = step;
+			this.sliders.appendChild(el);
+
+			// change listener
+			el.onchange = () => {
+				let res = set(el.value);
+				label.innerText = id + ": " + getRoundedValue(res);
+			}
+			el.value = default_val;
+			el.onchange();
+
+			this.sliders.className = "";
+
+			function getRoundedValue(val) {
+				if (val > 1e-1) {
+					return Math.round(val * 1000) / 1000
+				}
+				else if (val > 1e-2) {
+					return Math.round(val * 10000) / 10000
+				}
+				else if (val > 1e-3) {
+					return Math.round(val * 100000) / 100000
+				}
+				else if (val > 1e-4) {
+					return Math.round(val * 1000000) / 1000000
+				}
+				else if (val > 1e-5) {
+					return Math.round(val * 10000000) / 10000000
+				}
+				else if (val > 1e-6) {
+					return Math.round(val * 100000000) / 100000000
+				}
+				else return val;
+			}
+		},
 		disable() {
+			this.sliders.innerHTML = "";
 			this.sliders.className = "disabled";
 		}
 	}
