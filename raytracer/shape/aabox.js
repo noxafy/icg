b2c7d7c160b27b7fb696615fc5648815a872561f
noxafy <hci@gmx.de>
2018-09-09 11:55:07 +0200

    Working aabox

--- raytracer/shape/aabox.js
+++ raytracer/shape/aabox.js
@@ -52,38 +52,43 @@ class AABox extends Shape {
 		let t0z = (B0.z - O.z) / D.z
 		let t1z = (B1.z - O.z) / D.z
 
-		let tmin = Infinity;
-		let point;
-		let norm;
-		let z0 = O.add(D.mul(t0z));
-		if (z0.x > B0.x && z0.x < B1.x && z0.y > B0.y && z0.y < B1.y) {
-			point = z0;
+		let normal = new Vector(-1, -1, -1);
+		if (t0x > t1x) {
+			let temp = t0x;
+			t0x = t1x;
+			t1x = temp;
+			normal.x = 1;
+		}
+		if (t0y > t1y) {
+			let temp = t0y;
+			t0y = t1y;
+			t1y = temp;
+			normal.y = 1;
+		}
+		if (t0z > t1z) {
+			let temp = t0z;
+			t0z = t1z;
+			t1z = temp;
+			normal.z = 1;
+		}
+		let tmin = t0x;
+		let tmax = t1x;
+		if (tmin > t1y || t0y > t1x) return null;
+		if (t0y > tmin) {
+			tmin = t0y;
+			normal.x = 0;
+		} else {
+			normal.y = 0;
+		}
+		if (t1y < tmax) tmax = t1y;
+		if (tmin > t1z || t0z > tmax) return null;
+		if (t0z > tmin) {
 			tmin = t0z;
-			norm = new Vector(0, 0, (D.z < 0) ? 1 : -1, 1);
+			normal.x = normal.y = 0;
+		} else {
+			normal.z = 0;
 		}
-
-		if (t0y < tmin) {
-			let y0 = O.add(D.mul(t0y));
-			if (y0.x > B0.x && y0.x < B1.x && y0.z > B0.z && y0.z < B1.z) {
-				point = y0;
-				tmin = t0y;
-				norm = new Vector(0, (D.y < 0) ? 1 : -1, 0, 1);
-			}
-		}
-
-		if (t0x < tmin) {
-			let x0 = O.add(D.mul(t0x));
-			if (x0.y > B0.y && x0.y < B1.y && x0.z > B0.z && x0.z < B1.z) {
-				point = x0;
-				tmin = t0x;
-				norm = new Vector((D.x < 0) ? 1 : -1, 0, 0, 1);
-			}
-		}
-
-		if (!point) {
-			return null;
-		}
-		return new Intersection(tmin, point, norm);
+		if (tmin < 0) return null;
+		return new Intersection(tmin, D.mul(tmin).add(O), normal);
 	}
-
 }
